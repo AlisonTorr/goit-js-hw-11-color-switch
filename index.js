ed109915b88a87d48a74c85e5792550f9a5f3f49
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-04-15 18:08:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const bodyRef = document.querySelector("body");
+const startBtn = document.querySelector("#start-btn");
+const stopBtn = document.querySelector("#stop-btn");
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const colorAdding = () => {
+  const colorIdx = randomIntegerFromInterval(0, colors.length);
+  bodyRef.style.backgroundColor = colors[colorIdx];
+};
+
+const onStart = () => {
+  const intervalid = setInterval(colorAdding, 1000);
+  startBtn.disabled = true;
+
+  return intervalid;
+};
+
+const onStop = () => {
+  bodyRef.style.backgroundColor = colors[0];
+  clearInterval(intervalid);
+  startBtn.disabled = false;
+};
+
+startBtn.addEventListener("click", onStart);
+stopBtn.addEventListener("click", onStop);
